c4299f1c8fc640961691343860f485a16537a7a3
Harsh Saraf <harshsaraf97@gmail.com>
2024-07-17 18:13:28 +0530

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from openai import OpenAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+no_recipes
+
+
+prompt="Show me 5 recipes for a dish with the following ingredients: chicken, potatoes, and carrots. Per recipe, list all the ingredients used"
+  
+messages=[
+   # {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+     {"role": "system", "content": prompt}
+   # {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
   ]
-)
-print(completion.choices[0].message)
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+  )
+
+print(completion.choices[0].message.content)
