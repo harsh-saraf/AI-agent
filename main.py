d305c4fe6ecb5efea47eaec1d42c4ed3385567fb
Harsh Saraf <harshsaraf97@gmail.com>
2024-07-18 15:39:26 +0530

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from openai import OpenAI
+from openai import OpenAI 
+import httpx
 client = OpenAI()
 
-no_recipes
+no_recipes = input ("No of recipes (for example, 5: ")
+ingredients = input ("List of ingredients (for example, chicken, potatoes, and carrot: ")
+filter = input("Filter (for example, vegetarian, vegan, or gluten-free: ")
 
+prompt1 = f"Show me {no_recipes} recipes for a dish with the following ingredients: {ingredients}. Per recipe, list all the ingredients used, no {filter}"
 
-prompt="Show me 5 recipes for a dish with the following ingredients: chicken, potatoes, and carrots. Per recipe, list all the ingredients used"
+#prompt="Show me 5 recipes for a dish with the following ingredients: chicken, potatoes, and carrots. Per recipe, list all the ingredients used"
   
 messages=[
    # {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-     {"role": "system", "content": prompt}
+     {"role": "system", "content": prompt1}
    # {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
   ]
 
@@ -17,4 +21,16 @@ completion = client.chat.completions.create(
     messages=messages
   )
 
+prompt1_result = completion.choices[0].message.content
+
+prompt2 = "Produce a shopping list for the generated recipes and please don't include ingredients that I already have."
+
+prompt2_result = f"{prompt1_result} {prompt2}"
+messages = [{"role": "user", "content": prompt2_result}]
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+)
+
+print(prompt1_result)
 print(completion.choices[0].message.content)
